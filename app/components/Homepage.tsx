df95601cc86ac98d0d12b6e57f37dea046df19f6
chayilDeborah <aniyikayeolamide3@gmail.com>
2025-05-20 12:25:53 +0100

    feat: update Homepage layout and change app title to CTech Weather App

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
-
+import SearchBar from "./SearchBar";
+import CityCards from "./CityCards";
 const Homepage = () => {
 	return (
 		<main className="flex justify-center bg-[#0A0016] relative overflow-hidden">
@@ -8,6 +9,17 @@ const Homepage = () => {
 			<section className="w-full relative z-10">
 				<div className="w-[95%] md:w-full mx-auto lg:max-w-[1280px]">
 					<Navbar />
+					{/* <div className="flex flex-col mt-[50px] w-[80%] ">
+						<h1 className="text-white text-[20px] font-extrabold">
+							Stay ahead with real-time weather updates from
+							around the world.
+							<br />
+							Powered by Climezy Technologies — making weather
+							tracking personal and fun.
+						</h1>
+					</div> */}
+					<SearchBar />
+					<CityCards />
 				</div>
 			</section>
 		</main>
